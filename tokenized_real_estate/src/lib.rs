9d8e6d04fac9efcbbbd079a96e88562a18a4c385
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2024-09-30 07:16:29 -0700

    Update lib.rs

--- tokenized_real_estate/src/lib.rs
+++ tokenized_real_estate/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Mint, Token, TokenAccount};
 
-// Declare your program ID
+//  program ID
 declare_id!("HaodcEgNjhjEfZVSDJc1TdWt58e5JUSyuZNENGBjyQve");
 
 #[program]
